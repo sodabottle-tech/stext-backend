586df407c754171de972dd3179b5977335a96788
Rajesh Vishwakarma <rajesh.vishwakarma@here.com>
2019-12-29 01:01:25 +0530

    map db config for gcp

--- src/main/java/com/sodabottle/stext/controllers/KeyController.java
+++ src/main/java/com/sodabottle/stext/controllers/KeyController.java
@@ -5,14 +5,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
